ba917327f1ab58fec970748a38d66d65107efbf8
Kate-Shepel <kateborichevskaya@gmail.com>
2023-06-17 23:51:07 +0200

    feat: add solutions for arrays tasks part 3

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -316,8 +316,9 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const positiveEl = arr.filter((item) => typeof item === 'number' && item > 0);
+  return positiveEl.length;
 }
 
 /**
@@ -333,8 +334,9 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const orderArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return arr.sort((a, b) => orderArr.indexOf(a) - orderArr.indexOf(b));
 }
 
 /**
